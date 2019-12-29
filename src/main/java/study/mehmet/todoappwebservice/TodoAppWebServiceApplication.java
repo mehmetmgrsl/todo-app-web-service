402ac015251d9bba1953262905f945328c73dc40
MehmetGursul <mehmetgursul@gmail.com>
2019-12-29 22:07:35 +0100

    Spring Boot project was created

---
+++ src/main/java/study/mehmet/todoappwebservice/TodoAppWebServiceApplication.java
@@ -0,0 +1,13 @@
+package study.mehmet.todoappwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/mehmet/todoappwebservice/TodoAppWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package study.mehmet.todoappwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
