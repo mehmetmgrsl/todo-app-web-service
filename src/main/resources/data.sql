c3c52cd1a1e53a92c9b26191633af521f0420942
MehmetGursul <mehmetgursul@gmail.com>
2020-03-03 22:00:16 +0100

    data were updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ insert into todo(id,username,description,target_date,is_done)
 values(10001, 'in28minutes', 'Learn JPA', sysdate(),false);
 
 insert into todo(id,username,description,target_date,is_done)
-values(10002, 'in28minutes', 'Learn JPA', sysdate(),false); 
+values(10002, 'in28minutes', 'Learn Spring', sysdate(),false); 
 
 insert into todo(id,username,description,target_date,is_done)
-values(10003, 'in28minutes', 'Learn JPA', sysdate(),false);
+values(10003, 'in28minutes', 'Learn Kotlin', sysdate(),false);
